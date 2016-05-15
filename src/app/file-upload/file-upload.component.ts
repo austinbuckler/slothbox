dec724ab4e2dd29deb208289950953568bdaa6b1
Austin Buckler <buckleraustin@gmail.com>
2016-05-15 14:00:11 -0700

    starting on the file-browser.

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,4 +1,5 @@
-import { Component, NgZone } from '@angular/core';
+import { Component, NgZone, ElementRef, Renderer } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { AngularFire } from 'angularfire2';
 import { UPLOAD_DIRECTIVES } from '../file-uploader/file-uploader.component';
 import { UserService } from '../user.service';
@@ -8,23 +9,47 @@ import { UserService } from '../user.service';
   selector: 'file-upload-box',
   templateUrl: 'file-upload.component.html',
   styleUrls: ['file-upload.component.css'],
-  directives: [UPLOAD_DIRECTIVES]
+  directives: [UPLOAD_DIRECTIVES, NgClass]
 })
 export class FileUploadComponent {
   zone: NgZone;
-  options: Object = {
-    url: 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media',
-    authToken: this.user.googleToken,
-    withCredentials: true
-  };
+  options: Object;
   uploadProgress: number = 0;
   fileUploadResponse: any[] = [];
+  classMap: {};
+  dragOverListener: any;
+  dragLeaveListener: any;
 
   constructor(
-    public af:AngularFire,
+    public el: ElementRef,
+    public renderer: Renderer,
+    public af: AngularFire,
     public user: UserService
   ) {
     this.zone = new NgZone({ enableLongStackTrace: false });
+    this.options = {
+      authToken: user.googleToken,
+      withCredentials: true,
+      url: 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media'
+    };
+    this.classMap = {
+      'droparea': true,
+      'droparea-hidden': true
+    };
+    var self = this;
+    this.dragOverListener = renderer.listenGlobal('document', 'dragover', (e) => {
+      var dt = e.originalEvent.dataTransfer;
+      if(dt.types != null && (dt.types.indexOf ? dt.types.indexOf('Files') != -1
+        : dt.types.contains('application/x-moz-file'))) {
+          self.classMap['droparea-hidden'] = false;
+          window.clearTimeout(self.dragTimer);
+      }
+    });
+    this.dragLeaveListener = renderer.listenGlobal('document', 'dragleave', (e) => {
+      self.dragTimer = window.setTimeout(function() {
+          self.classMap['droparea-hidden'] = true;
+        }, 25);
+    });
   }
 
   handleDropUpload(data):void {
@@ -46,5 +71,9 @@ export class FileUploadComponent {
 
     this.uploadProgress = Math.floor(uploaded / (total / 100));
   }
+
+  dragTimer: any;
+
+
 
 }
--- src/app/filebrowser/filebrowser.component.ts
+++ src/app/filebrowser/filebrowser.component.ts
@@ -3,6 +3,8 @@ import { Router, ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 import { AngularFire } from 'angularfire2';
 import { FileUploadComponent } from '../file-upload/file-upload.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+import { Http, HTTP_PROVIDERS, Headers } from '@angular/http';
+import { UserService } from '../user.service';
 
 @Component({
   moduleId: module.id,
@@ -13,17 +15,37 @@ import { SidebarComponent } from '../sidebar/sidebar.component';
     ROUTER_DIRECTIVES,
     FileUploadComponent,
     SidebarComponent
+  ],
+  providers: [
+    HTTP_PROVIDERS
   ]
 })
 export class FilebrowserComponent implements OnInit {
 
+  private dropboxFilesURL: string = 'https://content.dropboxapi.com/1/files/auto/';
+  private googleDriveFilesURL: string = 'https://www.googleapis.com/drive/v3/files';
+  public googleDriveFiles: Object;
+  public dropboxFiles: Object;
+
   constructor(
     private router: Router,
-    public af: AngularFire
+    public af: AngularFire,
+    public http: Http,
+    public user: UserService
   ) {
     if (af.auth.getAuth() == null) {
       router.navigate(['Login']);
     }
+    var driveHeaders = new Headers({
+      'Authorization': 'Bearer ' + user.googleToken
+    });
+    var self = this;
+    this.googleDriveFiles = http.get(this.googleDriveFilesURL, {
+      headers: driveHeaders
+    }).subscribe(res => {
+      return res;
+    });
+    console.log(this.googleDriveFiles);
   }
 
   ngOnInit() {}
