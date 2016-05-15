7d590fdb6faff5d492076e667637de3fa4944756
MatthewBaldock <matt.w.g.baldock@gmail.com>
2016-05-14 23:20:15 -0700

    and

--- src/app/cloud/dropbox-auth.component.ts
+++ src/app/cloud/dropbox-auth.component.ts
@@ -11,11 +11,17 @@ export class DropBoxComponent implements OnInit {
 
 getResponse;
 constructor(http:Http){
-
+http.get("https://www.dropbox.com/1/oauth2/authorize?response_type=token&client_id=jbqssj52us3hsh2&redirect_uri=http://localhost:4200/auth/dropbox").subscribe(res => {
+          this.getResponse = res.json();
+        });
 var creds = "appkey= jbqssj52us3hsh2";
 var headers = new Headers();
+headers.append('Authorization', 'Bearer ti-BUBtTRMgAAAAAAAAE9gsiya42ajS3XDRi1CzgnDAvKrjJzsHYFyTiOXjLipO7');
 headers.append('Content-Type', 'application/json');
-http.post('https://api.dropboxapi.com/2/files/list_folder',creds,{
+http.post('https://api.dropboxapi.com/2/oauth2/token', JSON.stringify(null),{
+ headers: headers
+}).subscribe(res=>{console.log(res);});
+http.post('https://api.dropboxapi.com/2/files/list_folder',JSON.stringify({"path":"/phone"}),{
  headers: headers
 }).subscribe(res => {
           this.getResponse = res.json();
